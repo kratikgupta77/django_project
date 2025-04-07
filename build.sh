61651ada820a05d8115c05f6bb4e4cc4afffc866
Kratik Gupta <kratik22252@iiitd.ac.in>
2025-04-07 10:15:20 +0000

    changes in requirement 2

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
+
+# Install system dependencies (Debian-based)
+apt-get update && apt-get install -y \
+    build-essential \
+    graphviz-dev \
+    libpq-dev \
+    python3-dev \
+    pkg-config \
+    git
+
+# Install Python build tools
+pip install --upgrade pip setuptools wheel Cython
+
+# Install Python dependencies
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate 
