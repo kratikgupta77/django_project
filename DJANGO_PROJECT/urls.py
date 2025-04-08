e517e9c5aa31124c6bb09b2bf3df5314f996aa6b
Kratik Gupta <kratik22252@iiitd.ac.in>
2025-04-08 12:01:10 +0000

    new one 3

--- DJANGO_PROJECT/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', include("social_media.urls")),  # Existing app for login/signup
-    path('', include("profile_app.urls")),  # Profile management app
-]
--- p2pmarketplace/urls.py
+++
@@ -1,7 +0,0 @@
-# p2pmarketplace/urls.py
-from django.urls import path
-from .views import api
-
-urlpatterns = [
-    path("api/", api, name="p2p_api"),
-]
